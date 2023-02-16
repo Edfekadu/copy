aa9e669a03433d5db6ff3817e05cf45e3740638a
Edfekadu <Edfekadu@yahoo.com>
2023-02-16 13:10:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a new line"
@@ -7,7 +6,6 @@
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+    puts("\"Programming is like building a multilingual puzzle')
     return (0);
-
 }
