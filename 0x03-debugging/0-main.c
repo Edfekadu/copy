06abf08dd0115fcaa499fa9bf9c81617462bde97
Edfekadu <Edfekadu@yahoo.com>
2023-02-24 09:28:02 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *Main -tests functions that prints if integer is positive or negative
+ * Return: 0
+ */
+it main(void)
+{
+int i;
+i =0;
+positive_or_negative(i);
+return (0);
+}
