f3c15738aeac43b859c6bcd08f5463e18f2a1a53
Edfekadu <Edfekadu@yahoo.com>
2023-02-26 03:19:54 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,11 @@
-int _putchar(c);
+int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_X10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int )
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int,int);
