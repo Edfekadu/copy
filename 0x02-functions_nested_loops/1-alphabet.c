26c7a02f8f24692d0ffe3826447bcec974efeaf7
Edfekadu <Edfekadu@yahoo.com>
2023-02-26 02:03:34 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ * /
+ void print _alphabet (void)
+ {
+ char c;
+ for (c='a';c<='z';c++)
+ {
+ _putchar(c);
+ }
+ _putchar('\n')
+ }
