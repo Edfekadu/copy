076168f7ef4b41c04179b7f69a7ff345cd32ccd6
Edfekadu <Edfekadu@yahoo.com>
2023-03-04 06:18:45 -0800

    Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,6 +9,7 @@ char *cap_string(char *s)
 {
 	int i = 0, j;
 	char a[] = " \t\n,;.!?\"(){}";
+
 	while (*(s + i))
 	{
 		if (*(s + i) >= 'a' && *(s + i) <= 'z')
@@ -21,3 +22,10 @@ char *cap_string(char *s)
 				{
 					if (a[j] == *(s + i - 1))
 						*(s + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
