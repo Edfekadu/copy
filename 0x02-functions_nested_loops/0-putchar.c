d027fffd1a2fbd7272f15ff5878774c2dc89b6b8
Edfekadu <Edfekadu@yahoo.com>
2023-03-04 08:11:56 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main-entry point
- *Return:Always 0
+ * main - Print _putchar
+ * Return: Always 0
  */
-int main(void) 
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
